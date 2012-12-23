806925b697096a6c19dbffb02b41a08333698a72
TomiToivio <tomi@seravo.fi>
2012-12-23 11:52:21 +0200

    How to exclude event category from admin?

--- training-buddypress.php
+++ training-buddypress.php
@@ -90,7 +90,7 @@ add_filter('pre_get_posts', 'tm_posts_for_trainings_author');
 
 function tm_exclude_trainings_posts($query) {
 if($query->is_admin) {
-	$query->set('cat', '-364');
+	$query->set('event-categories', '-364');
 	}
 	return $query;	
 }
