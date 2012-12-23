198dba4b3a471a6feed03beefd6179e32e0c0cd0
Tomi Toivio <tomi@seravo.fi>
2012-12-23 11:28:25 +0200

    Update training-buddypress.php

--- training-buddypress.php
+++ training-buddypress.php
@@ -90,7 +90,7 @@ add_filter('pre_get_posts', 'tm_posts_for_trainings_author');
 
 function tm_exclude_trainings_posts($query) {
 if($query->is_admin) {
-	$query = new WP_Query( 'cat=-386' );
+	$query->set('category', '-186');
 	}
 	return $query;	
 }
